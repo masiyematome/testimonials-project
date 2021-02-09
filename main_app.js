c824863501e0bc315a8f3d6190fa822b1b78c6e7
masiyematome <matomemasiye7@gmail.com>
2021-02-09 18:45:08 +0200

    A function that takes us to the previous testimonial with the click of the previous button

--- main_app.js
+++ main_app.js
@@ -1,59 +1,77 @@
-//Selectors
+//Selectors and variables
 
 const previousTestimonial = document.querySelector(".previous-testimonial");
 const nextTestimonial = document.querySelector(".next-testimonial");
-const userImage = document.querySelector(".user-image");
 const userName = document.querySelector(".user-name");
+const userImage = document.querySelector(".user-image");
 const userTestimony = document.querySelector(".user-testimony");
+
+let currentTestimonyIndex = 0;
 
 //Event Listeners
 
 
 //Functions
 
-function objectDeclaration(arrayOfTestimonials){
-    arrayOfTestimonials.forEach(testimonial => {
-        console.log(testimonial.nameOfUser);
-        console.log(testimonial.imageOfUser);
-        console.log(testimonial.testimonyOfUser);
-        console.log("\n");
-    })
+function assignValuesToElements(arrayOfTestimonials){
+    userName.innerText = arrayOfTestimonials[currentTestimonyIndex].nameOfUser;
+    userImage.src = arrayOfTestimonials[currentTestimonyIndex].imageOfUser;
+    userTestimony.innerText = arrayOfTestimonials[currentTestimonyIndex].testimonyOfUser;
+}
+
+function printTestimonials(arrayOfTestimonials){
+
+    assignValuesToElements(arrayOfTestimonials);
+
+    previousTestimonial.addEventListener("click" , () => {
+        if(currentTestimonyIndex == 0){
+            currentTestimonyIndex = arrayOfTestimonials.length-1;
+        }
+
+        else{
+            currentTestimonyIndex = currentTestimonyIndex - 1;
+        }
+
+        assignValuesToElements(arrayOfTestimonials);
+
+    });
+    
 }
 
 ~function objectInitialization(theArrayOfTestimonials){
     theArrayOfTestimonials = [
         {
             nameOfUser : "Alexa Mathon",
-            imageOfUser: "AlexaMathon.png",
+            imageOfUser: "pics/AlexaMathon.png",
             testimonyOfUser : "I am completely blown away. Wow what great service, I love it!",
         },
 
         {
             nameOfUser : "Andrew Simons",
-            imageOfUser: "AndrewSimons.jpg",
+            imageOfUser: "pics/AndrewSimons.jpg",
             testimonyOfUser : "I'm good to go. Just what I was looking for. It's just amazing.I like software more and more each day because it makes my life a lot easier",
         },
 
         {
             nameOfUser : "Masilo Rabanyane",
-            imageOfUser: "MasiloRabanyane.jpg",
+            imageOfUser: "pics/MasiloRabanyane.jpg",
             testimonyOfUser : "I have gotten at least 50 times the value from software. I will refer everyone I know. We can't understand how we've been living without software. I can't say enough about software.",
         },
 
         {
             nameOfUser : "Nora Salamon Toth",
-            imageOfUser: "NoraSalamonToth.jpg",
+            imageOfUser: "pics/NoraSalamonToth.jpg",
             testimonyOfUser : "This is simply unbelievable!",
         },
 
         {
             nameOfUser : "Samuel Maponya",
-            imageOfUser: "SamuelMaponya.jpg",
+            imageOfUser: "pics/SamuelMaponya.png",
             testimonyOfUser : "You guys rock! The very best. We've seen amazing results already. We've used software for the last five years.",
         }
     ]
 
-    objectDeclaration(theArrayOfTestimonials);
+    printTestimonials(theArrayOfTestimonials);
 
 }();
 
